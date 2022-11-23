1909b29afc34d3a27b5664fd7e4fbbfb2a0fb1cf
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2022-11-23 13:53:53 +0100

    Changed depricated function.

--- getLanguage.ts
+++ getLanguage.ts
@@ -7,6 +7,6 @@ export function getLanguage(element: HTMLElement): isoly.Language | undefined {
 }
 function convert(language: string): isoly.Language | undefined {
 	if (language && language != "unknown" && language.length >= 2)
-		language = language.substr(0, 2)
+		language = language.substring(0, 2)
 	return isoly.Language.is(language) ? language : undefined
 }
