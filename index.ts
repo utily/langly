58aafa40032dbd88e5245d65f7880bd2e4332502
Elias Eriksson <eliaseriksson95@gmail.com>
2023-07-03 13:32:06 +0200

    New imports (#32)
    * updated packages
    
    * imports

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ import { getLanguage } from "./getLanguage"
 import { Translate } from "./Translate"
 import { Translation } from "./Translation"
 import { Translations } from "./Translations"
+export * as langly from "./langly"
 
 export { getLanguage, create, Translate, Translation, Translations }
